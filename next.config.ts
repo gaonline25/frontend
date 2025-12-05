cdeb1d5aaa61f2f808ced950fa51ee8134f6f1fe
Sourav Mehta <kazuokaze00@gmail.com>
2025-12-06 03:41:23 +0530

    Updated next

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,43 @@
+// // import type { NextConfig } from "next";
+
+// // const nextConfig: NextConfig = {
+// //   /* config options here */
+// // };
+
+// // export default nextConfig;
+
 // import type { NextConfig } from "next";
 
 // const nextConfig: NextConfig = {
+//   // Bypass TypeScript type checking during build
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   // Bypass ESLint during build
+//   // eslint: {
+//   //   ignoreDuringBuilds: true,
+//   // },
 //   /* config options here */
 // };
 
 // export default nextConfig;
 
+
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Bypass TypeScript type checking during build
+  experimental: {
+    serverComponentsExternalPackages: [
+      "payload",
+      "pino",
+      "thread-stream",
+    ],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
-  // Bypass ESLint during build
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  /* config options here */
 };
 
 export default nextConfig;
