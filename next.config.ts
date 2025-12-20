fb44333c4c87b10e9e22cc7919b94e4a71addfe9
Sourav Mehta <kazuokaze00@gmail.com>
2025-12-20 13:23:37 +0530

    Added versel json file

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,43 @@
+// // // // import type { NextConfig } from "next";
+
+// // // // const nextConfig: NextConfig = {
+// // // //   /* config options here */
+// // // // };
+
+// // // // export default nextConfig;
+
 // // // import type { NextConfig } from "next";
 
 // // // const nextConfig: NextConfig = {
+// // //   // Bypass TypeScript type checking during build
+// // //   typescript: {
+// // //     ignoreBuildErrors: true,
+// // //   },
+// // //   // Bypass ESLint during build
+// // //   // eslint: {
+// // //   //   ignoreDuringBuilds: true,
+// // //   // },
 // // //   /* config options here */
 // // // };
 
 // // // export default nextConfig;
 
+
+
+
 // // import type { NextConfig } from "next";
 
 // // const nextConfig: NextConfig = {
-// //   // Bypass TypeScript type checking during build
+// //   experimental: {
+// //     serverComponentsExternalPackages: [
+// //       "payload",
+// //       "pino",
+// //       "thread-stream",
+// //     ],
+// //   },
 // //   typescript: {
 // //     ignoreBuildErrors: true,
 // //   },
-// //   // Bypass ESLint during build
-// //   // eslint: {
-// //   //   ignoreDuringBuilds: true,
-// //   // },
-// //   /* config options here */
 // // };
 
 // // export default nextConfig;
@@ -29,14 +49,31 @@
 
 // const nextConfig: NextConfig = {
 //   experimental: {
-//     serverComponentsExternalPackages: [
-//       "payload",
-//       "pino",
-//       "thread-stream",
-//     ],
+//     serverComponentsExternalPackages: ["payload", "pino", "thread-stream"],
 //   },
+
 //   typescript: {
 //     ignoreBuildErrors: true,
+//   },
+
+//   // 🔥 FORCE OLD URL FORMAT
+//   trailingSlash: true,
+
+//   async redirects() {
+//     return [
+//       // NON-WWW → WWW
+//       {
+//         source: "/:path*",
+//         has: [
+//           {
+//             type: "host",
+//             value: "goldfingersaesthetics.com",
+//           },
+//         ],
+//         destination: "https://www.goldfingersaesthetics.com/:path*",
+//         permanent: true,
+//       },
+//     ];
 //   },
 // };
 
@@ -56,25 +93,8 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
 
-  // 🔥 FORCE OLD URL FORMAT
-  trailingSlash: true,
-
-  async redirects() {
-    return [
-      // NON-WWW → WWW
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "goldfingersaesthetics.com",
-          },
-        ],
-        destination: "https://www.goldfingersaesthetics.com/:path*",
-        permanent: true,
-      },
-    ];
-  },
+  // ❌ REMOVE trailingSlash
+  // ❌ REMOVE redirects from here
 };
 
 export default nextConfig;
